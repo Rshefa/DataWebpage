3f0b3f1d9aae391384ad2e2ed54bb74d088209c6
Rshefa <114087782+Rshefa@users.noreply.github.com>
2023-12-10 22:29:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -17,14 +17,16 @@ fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(info =>{
         console.log(info);
-       const post = info.map(post =>{
+        const firstTen = info.slice(0, 10);
+
+        const post = firstTen.map(post => {
           return `
             <div>
               <h2> ${post.body}</h2>
               </div>
              `
        })
-             document.getElementById('post').innerHTML = users.join(" ");   
+             document.getElementById('post').innerHTML = post.join(" ");   
             
       })
       
